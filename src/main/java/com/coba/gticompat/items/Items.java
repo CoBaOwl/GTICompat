e35b2580320ea9e382e38d31b846e193a533f878
CoBa <coba31rus@gmail.com>
2025-06-17 13:13:02 +0300

    Nuclear hotfix -fix server crush

--- src/main/java/com/coba/gticompat/items/Items.java
+++ src/main/java/com/coba/gticompat/items/Items.java
@@ -2,6 +2,7 @@ package com.coba.gticompat.items;
 
 import gregtech.api.unification.material.Material;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 
 import java.util.ArrayList;
@@ -16,27 +17,27 @@ public class Items {
     public static final ItemRadioactive THORIUM_ROD_DEP = new ItemRadioactive("thorium_rod_dep", 1);
     public static final ItemRadioactive THORIUM_ROD_DUAL_DEP = new ItemRadioactive("thorium_rod_dual_dep", 1);
     public static final ItemRadioactive THORIUM_ROD_QUAD_DEP = new ItemRadioactive("thorium_rod_quad_dep", 1);
-    public static final ItemReactorThorium THORIUM_ROD = new ItemReactorThorium("thorium_rod", 1, 50000, 0.4F, 0, 0.25F, false, THORIUM_ROD_DEP.getDefaultInstance());
-    public static final ItemReactorThorium THORIUM_ROD_DUAL = new ItemReactorThorium("thorium_rod_dual", 2, 50000, 0.4F, 0, 0.25F, false, THORIUM_ROD_DUAL_DEP.getDefaultInstance());
-    public static final ItemReactorThorium THORIUM_ROD_QUAD = new ItemReactorThorium("thorium_rod_quad", 4, 50000, 0.4F, 0, 0.25F, false, THORIUM_ROD_QUAD_DEP.getDefaultInstance());
+    public static final ItemReactorThorium THORIUM_ROD = new ItemReactorThorium("thorium_rod", 1, 50000, 0.4F, 0, 0.25F, false, new ItemStack(THORIUM_ROD_DEP));
+    public static final ItemReactorThorium THORIUM_ROD_DUAL = new ItemReactorThorium("thorium_rod_dual", 2, 50000, 0.4F, 0, 0.25F, false, new ItemStack(THORIUM_ROD_DEP));
+    public static final ItemReactorThorium THORIUM_ROD_QUAD = new ItemReactorThorium("thorium_rod_quad", 4, 50000, 0.4F, 0, 0.25F, false, new ItemStack(THORIUM_ROD_DEP));
     public static final ItemRadioactive URANIUM_ROD_DEP = new ItemRadioactive("uranium_rod_dep", 1);
     public static final ItemRadioactive URANIUM_ROD_DUAL_DEP = new ItemRadioactive("uranium_rod_dual_dep", 1);
     public static final ItemRadioactive URANIUM_ROD_QUAD_DEP = new ItemRadioactive("uranium_rod_quad_dep", 1);
-    public static final ItemReactorThorium URANIUM_ROD = new ItemReactorThorium("uranium_rod", 1, 20000, 2F, 1, 1F, false, URANIUM_ROD_DEP.getDefaultInstance());
-    public static final ItemReactorThorium URANIUM_ROD_DUAL = new ItemReactorThorium("uranium_rod_dual", 2, 20000, 2F, 1, 1F, false, URANIUM_ROD_DUAL_DEP.getDefaultInstance());
-    public static final ItemReactorThorium URANIUM_ROD_QUAD = new ItemReactorThorium("uranium_rod_quad", 4, 20000, 2F, 1, 1F, false, URANIUM_ROD_QUAD_DEP.getDefaultInstance());
+    public static final ItemReactorThorium URANIUM_ROD = new ItemReactorThorium("uranium_rod", 1, 20000, 2F, 1, 1F, false, new ItemStack(URANIUM_ROD_DEP));
+    public static final ItemReactorThorium URANIUM_ROD_DUAL = new ItemReactorThorium("uranium_rod_dual", 2, 20000, 2F, 1, 1F, false, new ItemStack(URANIUM_ROD_DUAL_DEP));
+    public static final ItemReactorThorium URANIUM_ROD_QUAD = new ItemReactorThorium("uranium_rod_quad", 4, 20000, 2F, 1, 1F, false, new ItemStack(URANIUM_ROD_QUAD_DEP));
     public static final ItemRadioactive MOX_ROD_DEP = new ItemRadioactive("mox_rod_dep", 1);
     public static final ItemRadioactive MOX_ROD_DUAL_DEP = new ItemRadioactive("mox_rod_dual_dep", 1);
     public static final ItemRadioactive MOX_ROD_QUAD_DEP = new ItemRadioactive("mox_rod_quad_dep", 1);
-    public static final ItemReactorThorium MOX_ROD = new ItemReactorThorium("mox_rod", 1, 10000, 2F, 1, 1F, true, MOX_ROD_DEP.getDefaultInstance());
-    public static final ItemReactorThorium MOX_ROD_DUAL = new ItemReactorThorium("mox_rod_dual", 2, 10000, 2F, 1, 1F, true, MOX_ROD_DUAL_DEP.getDefaultInstance());
-    public static final ItemReactorThorium MOX_ROD_QUAD = new ItemReactorThorium("mox_rod_quad", 4, 10000, 2F, 1, 1F, true, MOX_ROD_QUAD_DEP.getDefaultInstance());
+    public static final ItemReactorThorium MOX_ROD = new ItemReactorThorium("mox_rod", 1, 10000, 2F, 1, 1F, true, new ItemStack(MOX_ROD_DEP));
+    public static final ItemReactorThorium MOX_ROD_DUAL = new ItemReactorThorium("mox_rod_dual", 2, 10000, 2F, 1, 1F, true, new ItemStack(MOX_ROD_DUAL_DEP));
+    public static final ItemReactorThorium MOX_ROD_QUAD = new ItemReactorThorium("mox_rod_quad", 4, 10000, 2F, 1, 1F, true, new ItemStack(MOX_ROD_QUAD_DEP));
     public static final ItemRadioactive NAQUADAH_ROD_DEP = new ItemRadioactive("naquadah_rod_dep", 1);
     public static final ItemRadioactive NAQUADAH_ROD_DUAL_DEP = new ItemRadioactive("naquadah_rod_dual_dep", 1);
     public static final ItemRadioactive NAQUADAH_ROD_QUAD_DEP = new ItemRadioactive("naquadah_rod_quad_dep", 1);
-    public static final ItemReactorThorium NAQUADAH_ROD = new ItemReactorThorium("naquadah_rod", 1, 100000, 2F, 1, 1F, true, NAQUADAH_ROD_DEP.getDefaultInstance());
-    public static final ItemReactorThorium NAQUADAH_ROD_DUAL = new ItemReactorThorium("naquadah_rod_dual", 2, 100000, 2F, 1, 1F, true, NAQUADAH_ROD_DUAL_DEP.getDefaultInstance());
-    public static final ItemReactorThorium NAQUADAH_ROD_QUAD = new ItemReactorThorium("naquadah_rod_quad", 4, 100000, 2F, 1, 1F, true, NAQUADAH_ROD_QUAD_DEP.getDefaultInstance());
+    public static final ItemReactorThorium NAQUADAH_ROD = new ItemReactorThorium("naquadah_rod", 1, 100000, 2F, 1, 1F, true, new ItemStack(NAQUADAH_ROD_DEP));
+    public static final ItemReactorThorium NAQUADAH_ROD_DUAL = new ItemReactorThorium("naquadah_rod_dual", 2, 100000, 2F, 1, 1F, true, new ItemStack(NAQUADAH_ROD_DUAL_DEP));
+    public static final ItemReactorThorium NAQUADAH_ROD_QUAD = new ItemReactorThorium("naquadah_rod_quad", 4, 100000, 2F, 1, 1F, true, new ItemStack(NAQUADAH_ROD_QUAD_DEP));
     public static final ItemRadioactive ENRICHED_THORIUM_FUEL = new ItemRadioactive("enriched_thorium_fuel", 1);
     public static final ItemRadioactive NAQUADAH_FUEL = new ItemRadioactive("naquadah_fuel", 1);
 
